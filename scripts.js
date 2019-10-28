9d97ee7a6958519d3ab700d3df2f21be530ca5ca
popka56 <popka56@github.com>
2019-10-28 18:59:18 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+//To Top Button
+var topButton = document.getElementById("topAnchor");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("topAnchor").style.visibility = "visible";
+  } else {
+    document.getElementById("topAnchor").style.visibility = "hidden";
+  }
+}
+
+function toTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Search exempel jag kan göra själv med lite jobb
+formElementName = document.getElementById('form-submit');
+
+function search(){
+    var words = document.getElementById('searchbar').value;
+    if(words == 'world'){
+        window.location.href='pageName.html';
+    }
+    else{
+        window.location.href='errorPageName.html';
+    }
+}
+
+formElementName.addEventListener('submit', search);
