685e3ecaac1bf40fcd50fe40abace40b1f98ed3c
popka56 <57103849+popka56@users.noreply.github.com>
2019-11-19 15:49:42 +0100

    Dm Tools V.1.0

--- scripts.js
+++ scripts.js
@@ -50,7 +50,7 @@ function generateNumber() {
     document.getElementById("numberResult").innerHTML = "Result: Please enter only positive integers!";
   }
   else if (max <= min) {
-    document.getElementById("numberResult").innerHTML = "Result: Please make sure the Max Value is higher than the Min Value!";
+    document.getElementById("numberResult").innerHTML = "Result: Max Value must be lower than the Min Value!";
   }
   else {
     document.getElementById("numberResult").innerHTML = "Result: " + Math.floor(Math.random() * (max - min + 1) + min);
@@ -325,17 +325,476 @@ function generateNpc() {
   }
 }
 
-/*===Search Example===*/
-formElementName = document.getElementById('form-submit');
+function generateCompanion() {
+  randomNameMinValue = Math.ceil(1);
+  randomNameMaxValue = Math.floor(20);
+  randomName = Math.floor(Math.random() * (randomNameMaxValue - randomNameMinValue + 1) + randomNameMinValue);
 
-function search() {
-  var words = document.getElementById('searchbar').value;
-  if (words == 'world') {
-    window.location.href = 'pageName.html';
+  randomRaceMinValue = Math.ceil(1);
+  randomRaceMaxValue = Math.floor(9);
+  randomRace = Math.floor(Math.random() * (randomRaceMaxValue - randomRaceMinValue + 1) + randomRaceMinValue);
+
+  randomAgeMinValue = Math.ceil(1);
+  randomAgeMaxValue = Math.floor(4);
+  randomAge = Math.floor(Math.random() * (randomAgeMaxValue - randomAgeMinValue + 1) + randomAgeMinValue);
+
+  randomSexMinValue = Math.ceil(1);
+  randomSexMaxValue = Math.floor(2);
+  randomSex = Math.floor(Math.random() * (randomSexMaxValue - randomSexMinValue + 1) + randomSexMinValue);
+
+  randomClassMinValue = Math.ceil(1);
+  randomClassMaxValue = Math.floor(6);
+  randomClass = Math.floor(Math.random() * (randomClassMaxValue - randomClassMinValue + 1) + randomClassMinValue);
+
+  randomPersonalityMinValue = Math.ceil(1);
+  randomPersonalityMaxValue = Math.floor(10);
+  randomPersonality = Math.floor(Math.random() * (randomPersonalityMaxValue - randomPersonalityMinValue + 1) + randomPersonalityMinValue);
+
+  randomOriginMinValue = Math.ceil(1);
+  randomOriginMaxValue = Math.floor(7);
+  randomOrigin = Math.floor(Math.random() * (randomOriginMaxValue - randomOriginMinValue + 1) + randomOriginMinValue);
+
+  randomGoalMinValue = Math.ceil(1);
+  randomGoalMaxValue = Math.floor(15);
+  randomGoal = Math.floor(Math.random() * (randomGoalMaxValue - randomGoalMinValue + 1) + randomGoalMinValue);
+
+  if (randomOrigin == 7 && randomRace != 9) {
+    document.getElementById("companionRace").innerHTML = "Race: Saldi";
+  } else {
+    switch (randomRace) {
+      case 1:
+        document.getElementById("companionRace").innerHTML = "Race: Saldi";
+        break;
+      case 2:
+        document.getElementById("companionRace").innerHTML = "Race: Noble Elf";
+        break;
+      case 3:
+        document.getElementById("companionRace").innerHTML = "Race: Elf";
+        break;
+      case 4:
+        document.getElementById("companionRace").innerHTML = "Race: Half-Elf";
+        break;
+      case 5:
+        document.getElementById("companionRace").innerHTML = "Race: Dwarf";
+        break;
+      case 6:
+        document.getElementById("companionRace").innerHTML = "Race: Dragonfolk";
+        break;
+      case 7:
+        document.getElementById("companionRace").innerHTML = "Race: Halfling";
+        break;
+      case 8:
+        document.getElementById("companionRace").innerHTML = "Race: Feline";
+        break;
+      case 9:
+        document.getElementById("companionRace").innerHTML = "Race: Demonkin";
+        break;
+    }
+  }
+
+  switch (randomAge) {
+    case 1:
+      document.getElementById("companionAge").innerHTML = "Age: Young";
+      break;
+    case 2:
+      document.getElementById("companionAge").innerHTML = "Age: Young";
+      break;
+    case 3:
+      document.getElementById("companionAge").innerHTML = "Age: Middle-aged";
+      break;
+    case 4:
+      document.getElementById("companionAge").innerHTML = "Age: Old";
+      break;
+  }
+
+  switch (randomPersonality) {
+    case 1:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Jolly";
+      break;
+    case 2:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Grumpy";
+      break;
+    case 3:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Stoic";
+      break;
+    case 4:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Charming";
+      break;
+    case 5:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Gluttonous";
+      break;
+    case 6:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Loyal";
+      break;
+    case 7:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Greedy";
+      break;
+    case 8:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Scaredy-cat";
+      break;
+    case 9:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Solemn";
+      break;
+    case 10:
+      document.getElementById("companionPersonality").innerHTML = "Personality Trait: Naive";
+      break;
+  }
+
+  switch (randomGoal) {
+    case 1:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Exploring all the corners of the world.";
+      break;
+    case 2:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Finding true love in an unexpected place.";
+      break;
+    case 3:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Becoming a world-famous adventurer.";
+      break;
+    case 4:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Finding a worthy opponent/rival.";
+      break;
+    case 5:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Slaying an infamous monster.";
+      break;
+    case 6:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Solving an infamous mystery.";
+      break;
+    case 7:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Finding out the truth of their past.";
+      break;
+    case 8:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Become the greatest of their fighting style.";
+      break;
+    case 9:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Saving as many lives as they can.";
+      break;
+    case 10:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Get filthy rich.";
+      break;
+    case 11:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Avenging their lost family/friend(s).";
+      break;
+    case 12:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Meet the gods of the realm.";
+      break;
+    case 13:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Establish their own country/kingdom.";
+      break;
+    case 14:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Meeting/reuniting with their hero/master.";
+      break;
+    case 15:
+      document.getElementById("companionGoal").innerHTML = "Life Goal: Ridding the world of evil.";
+      break;
+  }
+
+  switch (randomSex) {
+    case 1:
+      document.getElementById("companionSex").innerHTML = "Sex: M";
+      break;
+    case 2:
+      document.getElementById("companionSex").innerHTML = "Sex: F";
+      break;
+  }
+
+  if (randomOrigin == 7 && randomRace != 9) {
+    switch (randomClass) {
+      case 1:
+        document.getElementById("companionClass").innerHTML = "Class: Artillery (Ashigaru, Red Monk).";
+        break;
+      case 2:
+        document.getElementById("companionClass").innerHTML = "Class: Brute (Ronin, Monk).";
+        break;
+      case 3:
+        document.getElementById("companionClass").innerHTML = "Class: Controller (Beast-tamer, Red Monk).";
+        break;
+      case 4:
+        document.getElementById("companionClass").innerHTML = "Class: Lurker (Shinobi, Ashigaru).";
+        break;
+      case 5:
+        document.getElementById("companionClass").innerHTML = "Class: Skirmisher (Monk, Beast-tamer).";
+        break;
+      case 6:
+        document.getElementById("companionClass").innerHTML = "Class: Soldier (Ronin, Shinobi).";
+        break;
+    }
   }
   else {
-    window.location.href = 'errorPageName.html';
+    switch (randomClass) {
+      case 1:
+        document.getElementById("companionClass").innerHTML = "Class: Artillery (Ranger, Wizard).";
+        break;
+      case 2:
+        document.getElementById("companionClass").innerHTML = "Class: Brute (Barbarian, Fighter).";
+        break;
+      case 3:
+        document.getElementById("companionClass").innerHTML = "Class: Controller (Warlord, Bard).";
+        break;
+      case 4:
+        document.getElementById("companionClass").innerHTML = "Class: Lurker (Rogue, Assassin).";
+        break;
+      case 5:
+        document.getElementById("companionClass").innerHTML = "Class: Skirmisher (Druid, Warlock).";
+        break;
+      case 6:
+        document.getElementById("companionClass").innerHTML = "Class: Soldier (Cleric, Paladin).";
+        break;
+    }
+  }
+
+
+  if (randomRace == 9) {
+    switch (randomOrigin) {
+      case 1:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Inferno.";
+        break;
+      case 2:
+        document.getElementById("companionOrigin").innerHTML = "Origin: The Depths.";
+        break;
+      case 3:
+        document.getElementById("companionOrigin").innerHTML = "Origin: The Ruins of Pandemonium.";
+        break;
+      case 4:
+        document.getElementById("companionOrigin").innerHTML = "Origin: The Vile Soil.";
+        break;
+      case 5:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Domain of Flames.";
+        break;
+      case 6:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Realm of the Depraved.";
+        break;
+      case 7:
+        document.getElementById("companionOrigin").innerHTML = "Origin: The Fiendish Grounds.";
+        break;
+    }
+  } else {
+    switch (randomOrigin) {
+      case 1:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Kirmer.";
+        break;
+      case 2:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Skriiod.";
+        break;
+      case 3:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Gircish.";
+        break;
+      case 4:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Oqpa.";
+        break;
+      case 5:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Sissaro.";
+        break;
+      case 6:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Wusari.";
+        break;
+      case 7:
+        document.getElementById("companionOrigin").innerHTML = "Origin: Na'ak.";
+        break;
+    }
+  }
+
+  if (randomOrigin == 7 && randomRace != 9) {
+    switch (randomName) {
+      case 1:
+        document.getElementById("companionName").innerHTML = "Name: Kanda";
+        break;
+      case 2:
+        document.getElementById("companionName").innerHTML = "Name: Okayamachi";
+        break;
+      case 3:
+        document.getElementById("companionName").innerHTML = "Name: Koyoen";
+        break;
+      case 4:
+        document.getElementById("companionName").innerHTML = "Name: Umeda";
+        break;
+      case 5:
+        document.getElementById("companionName").innerHTML = "Name: Ueno";
+        break;
+      case 6:
+        document.getElementById("companionName").innerHTML = "Name: Shimbashi";
+        break;
+      case 7:
+        document.getElementById("companionName").innerHTML = "Name: Shinagawa";
+        break;
+      case 8:
+        document.getElementById("companionName").innerHTML = "Name: Shinjuku";
+        break;
+      case 9:
+        document.getElementById("companionName").innerHTML = "Name: Ikebukuro";
+        break;
+      case 10:
+        document.getElementById("companionName").innerHTML = "Name: Nagita";
+        break;
+      case 11:
+        document.getElementById("companionName").innerHTML = "Name: Haneda";
+        break;
+      case 12:
+        document.getElementById("companionName").innerHTML = "Name: Roppongi";
+        break;
+      case 13:
+        document.getElementById("companionName").innerHTML = "Name: Ginza";
+        break;
+      case 14:
+        document.getElementById("companionName").innerHTML = "Name: Yamanote";
+        break;
+      case 15:
+        document.getElementById("companionName").innerHTML = "Name: Asakusa";
+        break;
+      case 16:
+        document.getElementById("companionName").innerHTML = "Name: Nikko";
+        break;
+      case 17:
+        document.getElementById("companionName").innerHTML = "Name: Nishi-Shin";
+        break;
+      case 18:
+        document.getElementById("companionName").innerHTML = "Name: Uji";
+        break;
+      case 19:
+        document.getElementById("companionName").innerHTML = "Name: Kobe";
+        break;
+      case 20:
+        document.getElementById("companionName").innerHTML = "Name: Nishinomiya";
+        break;
+    }
+  }
+  else if (randomSex == 1) {
+    switch (randomName) {
+      case 1:
+        document.getElementById("companionName").innerHTML = "Name: Ian";
+        break;
+      case 2:
+        document.getElementById("companionName").innerHTML = "Name: Noa";
+        break;
+      case 3:
+        document.getElementById("companionName").innerHTML = "Name: Dan";
+        break;
+      case 4:
+        document.getElementById("companionName").innerHTML = "Name: Feo";
+        break;
+      case 5:
+        document.getElementById("companionName").innerHTML = "Name: Prelgar";
+        break;
+      case 6:
+        document.getElementById("companionName").innerHTML = "Name: Fhudor";
+        break;
+      case 7:
+        document.getElementById("companionName").innerHTML = "Name: Agnacious";
+        break;
+      case 8:
+        document.getElementById("companionName").innerHTML = "Name: Kindal";
+        break;
+      case 9:
+        document.getElementById("companionName").innerHTML = "Name: Viggo";
+        break;
+      case 10:
+        document.getElementById("companionName").innerHTML = "Name: Q'ert";
+        break;
+      case 11:
+        document.getElementById("companionName").innerHTML = "Name: Ythir";
+        break;
+      case 12:
+        document.getElementById("companionName").innerHTML = "Name: Banman";
+        break;
+      case 13:
+        document.getElementById("companionName").innerHTML = "Name: Ernsti";
+        break;
+      case 14:
+        document.getElementById("companionName").innerHTML = "Name: Jergo";
+        break;
+      case 15:
+        document.getElementById("companionName").innerHTML = "Name: Churmi";
+        break;
+      case 16:
+        document.getElementById("companionName").innerHTML = "Name: Shu";
+        break;
+      case 17:
+        document.getElementById("companionName").innerHTML = "Name: Lo'Bavn";
+        break;
+      case 18:
+        document.getElementById("companionName").innerHTML = "Name: Ferdi";
+        break;
+      case 19:
+        document.getElementById("companionName").innerHTML = "Name: Mankio";
+        break;
+      case 20:
+        document.getElementById("companionName").innerHTML = "Name: Tjen";
+        break;
+    }
+  }
+  else {
+    switch (randomName) {
+      case 1:
+        document.getElementById("companionName").innerHTML = "Name: Mia";
+        break;
+      case 2:
+        document.getElementById("companionName").innerHTML = "Name: Emma";
+        break;
+      case 3:
+        document.getElementById("companionName").innerHTML = "Name: Tanya";
+        break;
+      case 4:
+        document.getElementById("companionName").innerHTML = "Name: Vayka";
+        break;
+      case 5:
+        document.getElementById("companionName").innerHTML = "Name: Rein";
+        break;
+      case 6:
+        document.getElementById("companionName").innerHTML = "Name: Eï";
+        break;
+      case 7:
+        document.getElementById("companionName").innerHTML = "Name: Ne'Sul";
+        break;
+      case 8:
+        document.getElementById("companionName").innerHTML = "Name: Antha";
+        break;
+      case 9:
+        document.getElementById("companionName").innerHTML = "Name: Lasva";
+        break;
+      case 10:
+        document.getElementById("companionName").innerHTML = "Name: Yorna";
+        break;
+      case 11:
+        document.getElementById("companionName").innerHTML = "Name: Zöé";
+        break;
+      case 12:
+        document.getElementById("companionName").innerHTML = "Name: Vertalonika";
+        break;
+      case 13:
+        document.getElementById("companionName").innerHTML = "Name: Kattla";
+        break;
+      case 14:
+        document.getElementById("companionName").innerHTML = "Name: Mae";
+        break;
+      case 15:
+        document.getElementById("companionName").innerHTML = "Name: Pafiga";
+        break;
+      case 16:
+        document.getElementById("companionName").innerHTML = "Name: Nisogufa";
+        break;
+      case 17:
+        document.getElementById("companionName").innerHTML = "Name: Ciri";
+        break;
+      case 18:
+        document.getElementById("companionName").innerHTML = "Name: Falala";
+        break;
+      case 19:
+        document.getElementById("companionName").innerHTML = "Name: Sina";
+        break;
+      case 20:
+        document.getElementById("companionName").innerHTML = "Name: Saravana";
+        break;
+    }
   }
 }
 
-formElementName.addEventListener('submit', search);
+/*===Search Test===*/
+function search() {
+  var words = document.getElementById('searchBar').value;
+
+  if (words == 'world') {
+    window.location.href = 'https://thorczhia.com/world.html';
+  }
+  else {
+    window.location.href = 'https://thorczhia.com/404.html';
+  }
+}
